ead1fa92ace6dabb23c6056a3d102267277b146b
Mitch Palczewski <135446256+mitchp819@users.noreply.github.com>
2025-04-05 11:53:03 -0400

    Home BG

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,20 @@
 <script>
-	import Card from '../lib/components/project_card.svelte';
-	import bg_image from 'static\main-assets\wavefunctionmountains.png';
-	let { data } = $props();
+    import Card from '../lib/components/project_card.svelte';
+    export let data;
 </script>
 
-<div class="h-screen bg-cover bg-center" style="background-image: url({bg_image})">
-	<h1>Home</h1>
-	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-	
-	{#each data.summaries as { name, image, description, link }}
-		<Card
-			title={ name }
-			description={description}
-			imageUrl={image}
-			link={link}
-		/>
-	{/each}
+<div class="h-screen bg-cover bg-top" style="background-image: url('/main_assets/wavefunctionmountains.png')">
+    <h1>Home</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    
+    {#each data.summaries as { name, image, description, link }}
+        <Card
+            title={name}
+            description={description}
+            imageUrl={image}
+            link={link}
+        />
+    {/each}
 </div>
 
 
