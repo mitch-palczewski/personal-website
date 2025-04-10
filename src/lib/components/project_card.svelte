ab76bdb295c2c4d5ad3a7e0a4810a29faca0948b
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-04-10 15:03:55 -0400

    mainpagecardhover

--- src/lib/components/project_card.svelte
+++ src/lib/components/project_card.svelte
@@ -8,7 +8,7 @@
     export let htmlContent; 
 </script>
 
-<div class="card m-8 max-w-xs border border-gray-300 p-4 text-center shadow-md {bg_color}">
+<div class="card m-8 max-w-xs border border-gray-300 p-4 text-center shadow-md {bg_color} hover:bg-orange-100 transition-colors duration-300">
     <a href={link} target="_blank" rel="noopener" class="block">
         <h2 class="text-right">{date}</h2>
         <img src={imageUrl} alt={title} class="card-image h-auto w-full" />
@@ -18,8 +18,6 @@
             <div class="text-left text-gray-600">
                 {@html htmlContent} 
             </div>
-            
-
         </div>
     </a>
 </div>
