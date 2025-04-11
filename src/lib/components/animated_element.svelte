ef49fe2e1430bce0a0475c8f076145c12a071a68
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-04-11 14:14:17 -0400

    p1Final1animation

--- src/lib/components/animated_element.svelte
+++ src/lib/components/animated_element.svelte
@@ -2,16 +2,20 @@
     export let color = '#fff0d4';
     export let width = '200px';
     export let height = '150px';
-    export let bottom = '30%';
-    export let left = '4%';
+	export let top = '';
+    export let bottom = '';
+    export let left = '';
+	export let right = '';
     export let h_time = '0s';
     export let h_dist = '0px';
     export let v_time = '0s';
     export let v_dist = '0px';
+	export let z_index = '1';
     export let image = null;
 
     $: movement = determineMovement();
     $: computedClass = movement === 'both' ? 'diagonal-move' : movement === 'horizontal' ? 'h-move' : movement === 'vertical' ? 'v-move' : '';
+
 
     function determineMovement() {
         if ((v_time === '0s' || v_dist === '0px') && (h_time === '0s' || h_dist === '0px')) {
@@ -28,15 +32,18 @@
 
 <div
     class="absolute {computedClass}"
-    style="bottom: {bottom}; left: {left}; 
-        --h-time: {h_time}; --h-dist: {h_dist}; 
-        --v-time: {v_time}; --v-dist: {v_dist}; 
-        width: {width}; height: {height};"
+    style=
+		"bottom: 	{bottom};	top: 		{top};
+		left: 		{left};  	right: 		{right};
+        --h-time: 	{h_time}; 	--h-dist: 	{h_dist}; 
+        --v-time: 	{v_time}; 	--v-dist: 	{v_dist}; 
+        width: 		{width}; 	height: 	{height};
+		z-index: 	{z_index};"
 >
     {#if image}
-        <img src={image} alt="Animated Element" style="width: 100%; height: 100%; object-fit: cover;" />
+        <img src={image} alt="Animated Element" style="width: 100%; height: 100%; object-fit: cover; z-index: {z_index};" />
     {:else}
-        <div style="background-color: {color}; width: 100%; height: 100%;"></div>
+        <div style="background-color: {color}; width: 100%; height: 100%; z-index: {z_index};"></div>
     {/if}
 </div>
 
--- src/routes/projects/painting-series-11-24/p1/+page.svelte
+++ src/routes/projects/painting-series-11-24/p1/+page.svelte
@@ -1,53 +1,49 @@
 <script>
-    import AnimatedElement from '$lib/components/animated_element.svelte';
+	import AnimatedElement from '$lib/components/animated_element.svelte';
 </script>
 
-
 <div class="relative flex h-screen items-center justify-center bg-[#393939]">
-	<img
-		src="\project-assets\paintseries11-24\Final1.png"
-		alt=""
-		class="card-image h-auto max-h-full"
-		style="width: 70%; height: auto; z-index: 5;"
-	/>
-
-	<div
-		class="absolute bg-[#bebebe]"
-		style="bottom: 10%; right: 5%; width: 200px; height: 300px; z-index: 1;"
-	></div>
-
+    <img
+        src="\project-assets\paintseries11-24\Final1.png"
+        alt=""
+        class="card-image h-auto max-h-full"
+        style="width: 75%; height: auto; z-index: 5; transform: translateX(1%);"
+    />
 	<div
 		class="absolute bg-[#8b8686]"
 		style="bottom: 0%; left: 5%; width: 90px; height: 700px; z-index: 1;"
 	></div>
 
-    <!-- Animated Orange
-	<div
-		class="absolute bg-[#fff0d4] h-move"
-		style="bottom: 30%; left: 4%; width: 200px; height: 150px; z-index: 2; --time: 30s; --dist: 200px"
-	></div> -->
-    <AnimatedElement
-    color="#ff0000"
-    width="150px"
-    height="150px"
-    bottom="20%"
-    left="10%"
-	h_time="5s"
-    h_dist="300px"
-    v_time="2s"
-    v_dist="20px"
-/>
-<AnimatedElement
-    color="#ff0000"
-    width="100px"
-    height="100px"
-    bottom="20%"
-    left="10%"
-    h_time="5s"
-    h_dist="300px"
-    v_time="5s"
-    v_dist="200px"
-/>
+	<AnimatedElement
+		color="#bebebe"
+		width="200px"
+		height="300px"
+		bottom="25.3%"
+		right="5%"
+		v_time="80s"
+		v_dist="200px"
+		z_index="3"
+	/>
+
+	<AnimatedElement
+		color="#9e9dba"
+		width="200px"
+		height="300px"
+		bottom="5%"
+		left="20%"
+		h_time="30s"
+		h_dist="600px"
+		z_index="2"
+	/>
+
+	<AnimatedElement
+		color="#fff0d4"
+		width="200px"
+		height="150px"
+		bottom="30%%"
+		left="4%"
+		h_time="50s"
+		h_dist="100px"
+		z_index="2"
+	/>
 </div>
-
-
