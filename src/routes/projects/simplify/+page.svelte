e492ffedc9a4323cf4d39c2b27a18bc38445aa1a
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-04-26 00:12:58 -0400

    green center painting

--- src/routes/projects/simplify/+page.svelte
+++ src/routes/projects/simplify/+page.svelte
@@ -1,14 +1,17 @@
+<script>
+	import Nav from '$lib/components/header.svelte';
+</script>
 
-    <img src="/project-assets/simplify/banner.png" alt="Title" class=" object-contain" />
+<Nav></Nav>
 
-<iframe
-    title="Simplify Game Embed"
-    frameborder="0"
-    src="https://itch.io/embed/2220048"
-    width="552"
-    height="167"
->
-    <a href="https://mitchpalczewski.itch.io/simplify">
-        Simplify - An Art Decomposition Game by MitchPalczewski
-    </a>
-</iframe>
+<img src="/project-assets/simplify/banner.png" alt="Title" class=" object-contain p-1" />
+<div class="mx-2 border-4 border-orange-100 bg-white p-10 sm:mx-10 md:mx-50 lg:mx-70">
+	<iframe
+		class="h-[200px] w-full sm:h-[400px]"
+		src="https://youtube.com/embed/9V88gKdshZQ"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+		title="Next Brush Stroke Predictor Tutorial"
+	></iframe>
+</div>
