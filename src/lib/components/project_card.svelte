178dc26a903d9a712fc96b29d0115e88997b4dae
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-04-25 23:21:31 -0400

    cool cards

--- src/lib/components/project_card.svelte
+++ src/lib/components/project_card.svelte
@@ -8,17 +8,35 @@
 </script>
 
 <div
-        class="card group m-8 bg-[#FEF6FC] w-120 h-90 border-4 border-[#703622] p-4 text-center shadow-md transition-colors duration-300 hover:bg-[#226670] hover:border-[#CCB8B2] hover:w-130"
+	class="card group m-8 h-auto w-auto border-2 border-[#E3D373] bg-transparent text-centertransition-colors duration-300 
+    hover:shadow-2xl hover:shadow-[#342C63] hover:border-2 hover:bg-gradient-to-r hover:from-[#226670] hover:to-[#144B53] hover:border-[#CCB8B2] 
+    md:w-170 lg:w-230"
 >
-    <a href={link} rel="noopener" class="block">
-        <h2 class="text-right text-blue-900 group-hover:text-blue-100">{date}</h2>
-        <img src={imageUrl} alt={title} class="card-image w-full h-48 object-contain" />
-        <div class="card-content text-black group-hover:text-white">
-            <h2 class="my-2 text-xl font-semibold">{title}</h2>
-            <p class="text-left">{description}</p>
-            <div class="text-left group-hover:text-white">
-                {@html htmlContent}
-            </div>
-        </div>
-    </a>
+	<div class="border-5 border-transparent hover:border-[#CCB8B2]">
+		<div class="border-5 border-transparent hover:border-[#226670]">
+			<a href={link} rel="noopener" class="block">
+			<div class="grid-col-1 grid gap-0 md:grid-cols-2">
+				<!-- First Column -->
+				<div
+					class="border-7 border-[#C5795E] bg-[#FEF6FC] p-5 transition-colors shad
+                    duration-300 group-hover:border-transparent group-hover:bg-transparent "
+				>
+					<img src={imageUrl} alt={title} class="card-image h-48 w-full object-contain" />
+					<div class="card-content text-black group-hover:text-white">
+						<h2 class="my-2 text-3xl font-semibold">{title}</h2>
+						<p class="text-left">{description}</p>
+						<div class="text-left group-hover:text-white">
+							{@html htmlContent}
+						</div>
+					</div>
+				</div>
+
+				<!-- Second Column -->
+				<div class="bg-transparent">
+					<h2 class="p-5 text-right text-blue-900 group-hover:text-blue-50">{date}</h2>
+				</div>
+			</div>
+        </a>
+		</div>
+	</div>
 </div>
--- src/routes/projects/next-brush-stroke-predictor/dataset/+page.svelte
+++ src/routes/projects/next-brush-stroke-predictor/dataset/+page.svelte
@@ -2,12 +2,9 @@
 	type ImageModule = { default: string };
 
 	// Import all images with .jpg and .png extensions from the specified folder eagerly.
-	const imageModules = import.meta.glob(
-		'/src/lib/image-collections/nbsp-images/*.{jpg,png}',
-		{
-			eager: true
-		}
-	) as Record<string, ImageModule>;
+	const imageModules = import.meta.glob('/src/lib/image-collections/nbsp-images/*.{jpg,png}', {
+		eager: true
+	}) as Record<string, ImageModule>;
 
 	// Extract the default exports (i.e., the image URLs)
 	let images = Object.entries(imageModules).map(([_, module]) => module.default);
@@ -20,16 +17,16 @@
 		class=" p-5 font-medium text-blue-900 hover:underline dark:text-blue-800">Back</a
 	>
 </div>
-<div class="grid grid-cols-1 gap-4 bg-blue-500 p-5 sm:grid-cols-1 md:grid-cols-2">
-    {#each images as image, index}
-        <div class="relative">
-            <img
-                src={image}
-                alt="Image {index + 1}"
-                class="h-auto w-full shadow-md"
-                style="image-rendering: pixelated;"
-                loading="lazy"
-            />
-        </div>
-    {/each}
+<div class="grid grid-cols-1 gap-8 bg-blue-500 p-5 sm:grid-cols-1 md:grid-cols-3">
+	{#each images as image, index}
+		<div class="relative">
+			<img
+				src={image}
+				alt="Image {index + 1}"
+				class="h-auto w-full"
+				style="image-rendering: pixelated;"
+				loading="lazy"
+			/>
+		</div>
+	{/each}
 </div>
