592559dddda37f8932c6b0fb575e3d2aa41453a8
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-04-27 09:56:40 -0400

    simplify link to play

--- src/routes/projects/simplify/+page.svelte
+++ src/routes/projects/simplify/+page.svelte
@@ -3,15 +3,30 @@
 </script>
 
 <Nav></Nav>
-
-<img src="/project-assets/simplify/banner.png" alt="Title" class=" object-contain p-1" />
-<div class="mx-2 border-4 border-orange-100 bg-white p-10 sm:mx-10 md:mx-50 lg:mx-70">
-	<iframe
-		class="h-[200px] w-full sm:h-[400px]"
-		src="https://youtube.com/embed/9V88gKdshZQ"
-		frameborder="0"
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-		allowfullscreen
-		title="Next Brush Stroke Predictor Tutorial"
-	></iframe>
+<div class="bg-[#d8c6f3]">
+<img src="/project-assets/simplify/banner.png" alt="Title" class=" object-contain " />
+<div class="grid grid-cols-3 py-5 px-2">
+	<div class="col-span-2 border-4 border-orange-100 bg-white p-10">
+		<iframe
+			class="h-[200px] w-full sm:h-[400px]"
+			src="https://youtube.com/embed/9V88gKdshZQ"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+			title="Next Brush Stroke Predictor Tutorial"
+		></iframe>
+	</div>
+	
+	<div class="p-5">
+		<a
+			href="https://secure.hyperrhiz.io/mitchell/index.html"
+			target="_blank"
+			rel="noopener"
+			class=" block bg-[#53681a] h-[200px] sm:h-[400px] my-6 p-1 lg:p-5 py-10 text-right text-3xl lg:text-7xl font-bold text-white 
+					hover:bg-orange-300 hover:text-black hover:underline hover:border-4 hover:border-red-400"
+		>
+			Play Simplify on the web
+		</a>
+	</div>
+</div>
 </div>
