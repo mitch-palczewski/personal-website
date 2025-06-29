8c03b646dc7c3c52923d1570818ffd70d31f0fab
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-06-29 12:34:13 -0400

    about piepost

--- src/routes/piepost/+page.svelte
+++ src/routes/piepost/+page.svelte
@@ -7,9 +7,19 @@
 <PiePostHeader title="PiePost" subtitle=""></PiePostHeader>
 
 <div class="m-1 sm:m-5">
-	<div class="grid grid-cols-1 sm:grid-cols-2 gap-1">
+	<div class="grid grid-cols-1 gap-1 sm:grid-cols-2">
 		<div>
-			<img src="/project-assets/piepost/piepostexe.png" class="w-auto" alt="Pie Post Executable" />
+			<img src="/project-assets/piepost/my_piepost.jpeg" class="w-auto pb-3" alt="My Piepost" />
+			<img
+				src="/project-assets/piepost/piepostexe.png"
+				class="w-autopb-3"
+				alt="Pie Post Executable"
+			/>
+			<img
+				src="/project-assets/piepost/tutorial/s6.png"
+				class="w-autopb-3"
+				alt="Pie Post Executable"
+			/>
 		</div>
 		<div class="p-3">
 			<h1 class="text-lg font-bold">About PiePost</h1>
@@ -20,9 +30,42 @@
 				formating and deploying html. Upload your media, write a caption, and all it takes is a
 				click of "Build Post". Your website will be up and running.
 				<br />
+				<br />
 				With PiePost you have complete control over your data and your website. PiePost automates writing
 				code but the code is still html and easily editable. Your public data is stored in posts.json
-				which you have the freedom to edit and delete.
+				which you have the freedom to modify, edit and delete.
+				<br />
+				<a
+					href="https://mitch-palczewski.github.io/"
+					class="text-blue-600 underline transition-colors duration-200 hover:text-blue-800"
+				>
+					Example PiePost Website
+				</a>
+			</p>
+			<br />
+			<h1 class="text-lg font-bold">Why PiePost</h1>
+			<p>
+				PiePost is a decentralized social media environment, where profiles and feeds are modular
+				and free. What binds PiePosts accounts together is the structure of the posts.json data
+				attached to each website. That's it! There are no other requirements to be part of the
+				system. Users are free to creatively build upon their PiePost websites, integrate them into
+				there own environments, and even build there own feeds to host there personal communities.
+				Users host there data privately meaning there are no accounts, no adds, no servers, and no
+				one to tell you what to do.
+				<br />
+				<br />
+				PiePost is an environment based upon modular expansion, organic growth, and urban design principles.
+				It is social media that is adaptable and transformable. It can support both small and large communities,
+				and easily integrate into existing infrastructures. An environment must have some centralized
+				axioms to build structures and networks from but ideally they are few and simple. For PiePost
+				this axiom is the structure (posts.json) and naming conventions behind it.
+				<br />
+				<br />
+				Like train infrastructure there must be a defined and agreed upon rail and rail gauge (distance
+				between rails), but after that there can be many types and brands of locomotives and train cars
+				that serve a variety of purposes. Stations are customizable and lead to different destinations.
+				PiePost seeks to emulate this type of structure where there is some amount of standardization
+				but after that it can branch freely and be built organically to fit the needs of the community.
 			</p>
 			<br />
 			<a
