ee1b804678ec5b9d3194edd138ede78cfa746ebc
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-08-08 17:04:11 -0400

    web-vistas page1

---
+++ src/routes/projects/web_vistas/page1/+page.svelte
@@ -0,0 +1,134 @@
+<div class="overflow-x-hidden overflow-y-hidden bg-blue-50">
+	<div class="relative h-[300vh] w-[900vw]">
+		<img
+			src="\project-assets\web_vistas\page1\s1.svg"
+			alt="Icon 1"
+			class="absolute top-[0vh] left-[50vw]
+             w-[195vh]
+            
+             -translate-x-1/2 -translate-y-1/2 rotate-[270deg]
+             "
+		/>
+
+		<img
+			src="\project-assets\web_vistas\page1\s2.svg"
+			alt="Icon 2"
+			class="absolute top-[20vh] left-[50vw] w-[195vh]
+             -translate-x-3/4
+             -translate-y-1/2
+             rotate-[200deg]
+             "
+		/>
+		<!--P1-->
+		<img
+			src="\project-assets\web_vistas\page1\p1.svg"
+			alt="P1"
+			class="absolute top-[2vh] left-[20vw] z-30 w-[40vh]
+             -translate-x-1/5
+             sm:top-[5vh]
+             sm:w-[50vh] md:left-[30vw]
+             "
+		/>
+		<img
+			src="\project-assets\web_vistas\page1\s3.svg"
+			alt="Icon 1"
+			class="absolute top-[120vh] left-[15vw] w-[150vh]
+             -translate-x-1/5
+             -translate-y-1/2
+             rotate-[-70deg]
+             "
+		/>
+		<div
+			class="flex w-[90vw] flex-col gap-5
+                px-[5vw]
+                pt-[105vh]
+                md:flex-row md:pt-[120vh]"
+		>
+			<!--P2-->
+			<img
+				src="\project-assets\web_vistas\page1\p2.svg"
+				alt="P2"
+				class="
+                z-30
+                w-[25vh] lg:w-[40vh]"
+			/>
+			<!--P3-->
+			<img
+				src="\project-assets\web_vistas\page1\p3.svg"
+				alt="P3"
+				class="
+                z-30
+                w-[25vh] lg:w-[40vh]"
+			/>
+		</div>
+		<img
+			src="\project-assets\web_vistas\page1\s2.svg"
+			alt="Icon 1"
+			class="absolute top-[175vh] left-[50vw] w-[300vh]
+             -translate-x-1/2
+             -translate-y-1
+             rotate-[20deg]
+            "
+		/>
+		<img
+			src="\project-assets\web_vistas\page1\s1.svg"
+			alt="Icon 1"
+			class="sm: md: absolute top-[210vh] left-[120vw]
+             w-[195vh]
+             
+             -translate-x-1/2 -translate-y-1/2 rotate-[270deg]
+             "
+		/>
+		<!--P4-->
+		<img
+			src="\project-assets\web_vistas\page1\p4.svg"
+			alt="P4"
+			class=" absolute top-[210vh] left-[50vw] z-30
+             h-[80vh]
+             max-w-[90vw]
+             -translate-x-1/2
+             
+             "
+		/>
+
+		<img
+			src="\project-assets\web_vistas\page1\sq1.svg"
+			alt="Green box"
+			class="fixed top-[5vh] right-[3vw] z-20 h-[2vw]
+             -translate-x-1/2 -translate-y-1/2"
+		/>
+		<img
+			src="\project-assets\web_vistas\page1\sq2.svg"
+			alt="Target 1"
+			class="absolute top-[5vh] right-[801vw] z-10 h-[4vw]
+             -translate-x-1/2 -translate-y-1/2"
+		/>
+		<img
+			src="\project-assets\web_vistas\page1\sq2.svg"
+			alt="Target 1"
+			class="absolute top-[99vh] right-[801vw] z-10 h-[4vw]
+             -translate-x-1/2 -translate-y-1/2 scale-y-200"
+		/>
+		<img
+			src="\project-assets\web_vistas\page1\sq2.svg"
+			alt="Target 1"
+			class="absolute top-[205vh] right-[801vw] z-10 h-[4vw]
+             -translate-x-1/2 -translate-y-1/2"
+		/>
+		<!-- svelte-ignore a11y_consider_explicit_label -->
+		<a href="/">
+		<button
+			class="
+                absolute bottom-[2vh] left-[1vw]  bg-[#77E69A] border border-white
+                px-1 py-2 text-[#77E69A] transition-all duration-500 hover:px-12 hover:text-white
+            "
+		>
+			<svg class="fill-current" 
+			width="80" height="20" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+				<polygon points="0 50,200 100,200 0,100"  />
+			</svg>
+		</button>
+		</a>
+
+	</div>
+</div>
